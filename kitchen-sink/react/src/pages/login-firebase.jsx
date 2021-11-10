085f3bfbb440d80187578886fa2a28715e706dea
sjafru <afandy.cooz@gmail.com>
2021-11-10 09:25:14 +0700

    updat tab login signup

--- kitchen-sink/react/src/pages/login-firebase.jsx
+++ kitchen-sink/react/src/pages/login-firebase.jsx
@@ -21,6 +21,10 @@ export default () => {
   const [password, setPassword] = useState('');
   const [otp, setOtp] = useState('');
 
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dbo, setDbo] = useState('');
+
   const signIn = () => {
     f7.dialog.alert('Username: ' + username + '<br>Password: ' + password, () => {
       f7.loginScreen.close();
@@ -30,15 +34,59 @@ export default () => {
   return (
     <Page>
       <Navbar title="Masuk" backLink={true}>
-        <Button slot="right">Daftar</Button>
+        <Button slot="right" onClick={() => f7.tab.show('#tablogin-signup')}>
+          Daftar
+        </Button>
       </Navbar>
       <Tabs>
+        <Tab id="tablogin-signup">
+          <List form>
+            <ListInput
+              type="text"
+              name="username"
+              placeholder="Your No Handphone"
+              label="No Handphone"
+              value={username}
+              onInput={(e) => setUsername(e.target.value)}
+              floatingLabel={true}
+            ></ListInput>
+            <ListInput
+              type="text"
+              name="firstName"
+              placeholder=""
+              label="Nama Depan"
+              value={firstName}
+              onInput={(e) => setFirstName(e.target.value)}
+              floatingLabel={true}
+            ></ListInput>
+            <ListInput
+              type="text"
+              name="lastName"
+              placeholder=""
+              label="Nama Keluarga"
+              value={lastName}
+              onInput={(e) => setLastName(e.target.value)}
+              floatingLabel={true}
+            ></ListInput>
+            <ListInput
+              type="date"
+              name="dbo"
+              placeholder=""
+              label="Tanggal Lahir"
+              value={dbo}
+              onInput={(e) => setDbo(e.target.value)}
+              floatingLabel={true}
+            ></ListInput>
+            <ListButton title="Submit" onClick={() => {}} />
+            <ListButton title="Cancel" onClick={() => {}} />
+          </List>
+        </Tab>
         <Tab id="tablogin-username" tabActive>
           <List form>
             <ListInput
               type="text"
               name="username"
-              placeholder="Email atau No Handphone"
+              placeholder="No Handphone"
               value={username}
               onInput={(e) => setUsername(e.target.value)}
               outline
